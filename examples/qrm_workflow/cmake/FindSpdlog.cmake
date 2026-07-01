e8c1007edf96f79a0dcd15a359a2055b7a0b1dcf
akshay9594 <akshay9407@outlook.com>
2026-07-01 14:06:50 +0000

    [example] Refactoring of Submitter code
    - Submitter now uses on qdmi client APIs
    - Now fetching mqt-core as a cmake module
    - Linking mqt-core's qdmi driver
    - Dynamic loading of qdmi device libraries
    - Removed static loading of qdmi device libraries altogether
    - New target_compile_definitions entry for Submitter in CMakelists.txt

--- examples/qrm_workflow/cmake/FindSpdlog.cmake
+++ examples/qrm_workflow/cmake/FindSpdlog.cmake
@@ -1,23 +1,6 @@
-# Enable FetchContent module
-include(FetchContent)
-
-# Fetch spdlog using FetchContent
-FetchContent_Declare(
-  spdlog
-  GIT_REPOSITORY https://github.com/gabime/spdlog.git
-  GIT_TAG v1.12.0 # You can specify a version tag here
-)
-
-# Make sure spdlog is downloaded and available
-FetchContent_MakeAvailable(spdlog)
-
-if(TARGET spdlog)
-  install(
-    TARGETS spdlog
-    EXPORT spdlogConfig
-    LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/lib
-    ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/lib
-    RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/bin
-    INCLUDES
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/include)
+# cmake/Findspdlog.cmake  (yours, overrides the broken one via module path precedence)
+if(TARGET spdlog::spdlog)
+  set(spdlog_FOUND TRUE)
+  return()
 endif()
+find_package(spdlog CONFIG QUIET)
